906100d743f6a891fe374aab25991e5a64b7be14
Matsumoto Kotaro <e235737@eve.u-ryukyu.ac.jp>
2025-07-16 07:19:33 +0900

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,8 @@ import rehypeKatex from 'rehype-katex';
 import remarkMermaid from 'remark-mermaidjs';
 
 export default defineConfig({
+  site: 'https://my-portfolio-f4k.pages.dev',
+
   integrations: [
     react(),
     tailwind(),
@@ -17,15 +19,12 @@ export default defineConfig({
       remarkPlugins: [
         remarkMath,
         [remarkMermaid, {
-          // スタイリング用のラッパーを確実に生成する設定
           wrap: {
             tagName: 'div',
             className: 'mermaid-diagram-container'
           },
-          // カスタマイズのベースとなる 'base' テーマを指定
           theme: 'base',
           themeVariables: {
-            // 図の要素（ノード、線、文字）の色
             primaryColor: '#18181b',
             primaryTextColor: '#f3f4f6',
             primaryBorderColor: '#6366f1',
